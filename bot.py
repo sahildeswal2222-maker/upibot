a413d3e55c47dfe7e6b53037e32d22501bed5d6b
sahildeswal2222-maker <sahildeswal2222@gmail.com>
2025-11-03 23:27:26 +0530

    Update bot.py 11

--- bot.py
+++ bot.py
@@ -46,6 +46,20 @@ def main():
 
 if __name__ == "__main__":
     main()
+import threading
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run_flask():
+    app.run(host="0.0.0.0", port=10000)
+
+threading.Thread(target=run_flask).start()
+
 
 
 
